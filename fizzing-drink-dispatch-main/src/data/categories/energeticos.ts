48eb6f26e5982a41543edd7c604511e6b75485a2
Gustavei <166328903+Gustavei@users.noreply.github.com>
2025-02-04 15:48:50 -0300

    first commit

---
+++ fizzing-drink-dispatch-main/src/data/categories/energeticos.ts
@@ -0,0 +1,26 @@
+export const energeticos = [
+  {
+    name: "Monster Energy 473ml",
+    description: "Monster Energy 473ml - 6 Unidades - R$ 8,60",
+    price: 51.59,
+    image: "https://i.ibb.co/rG0VpnBZ/8.png",
+  },
+  {
+    name: "Monster Mango Loco 473ml",
+    description: "Monster Mango Loco 473ml - 6 Unidades - R$ 8,60",
+    price: 51.59,
+    image: "https://i.ibb.co/21sG2FDV/9.png",
+  },
+  {
+    name: "Água Tônica Antártica 350ml",
+    description: "Água Tônica Antártica 350ml - 12 Unidades - R$ 3,33",
+    price: 39.99,
+    image: "https://i.ibb.co/kqPRtxd/43.png",
+  },
+  {
+    name: "Schweppes Tônica 350ml",
+    description: "Schweppes Tônica 350ml - 6 Unidades - R$ 2,99",
+    price: 17.99,
+    image: "https://i.ibb.co/3NvNjmg/42.png",
+  },
+];
---
+++ fizzing-drink-dispatch-main/src/data/categories/isotonicos.ts
@@ -0,0 +1,8 @@
+export const isotonicos = [
+  {
+    name: "Gatorade Sabor Uva 500ml",
+    description: "Gatorade Sabor Uva 500ml - 6 Unidades - R$ 5,32",
+    price: 31.90,
+    image: "https://i.ibb.co/cpMTXv0/6.png",
+  },
+];
